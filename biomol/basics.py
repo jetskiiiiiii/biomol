962b5bc8e0cf6565f77f2d05c7253414fdbc5927
Treverrio Primandaru <jetskiiiiiii22@gmail.com>
2024-11-19 20:03:11 -0800

    Transcribe DNA into RNA

--- biomol/basics.py
+++ biomol/basics.py
@@ -12,3 +12,16 @@ def count_dna_nucleotides(sequence: str) -> List[int]:
         bases[s] += 1
         
     return bases
+
+def transcribe_dna_into_rna(sequence: str) -> str:
+    """
+    An RNA string is a string formed from the alphabet containing 'A', 'C', 'G', and 'U'.
+
+    Given a DNA string t corresponding to a coding strand, its transcribed RNA string u
+    is formed by replacing all occurrences of 'T' in t with 'U' in u.
+
+    Given: A DNA string t having length at most 1000 nt.
+    Return: The transcribed RNA string of t.
+    """
+    return ''.join([s if s != 'T' else 'U' for s in sequence])
+
